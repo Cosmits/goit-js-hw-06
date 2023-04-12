eb980c69f54b61cb033c1df9e09338852f4a7685
Cosmits <salge.cos@gmail.com>
2023-04-13 00:43:46 +0200

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
 
+const formEl = document.querySelector('.login-form')
+
+formEl.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const login = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (login && password ) {
+
+        const formData = {
+            login,
+            password,
+        }
+        console.log(formData)
+
+        formEl.reset();
+    }
+    else {
+        alert('Всі поля повинні бути заповнені.')
+    }
+});
