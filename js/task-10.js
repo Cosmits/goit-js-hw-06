36b84643c2f9d27df5f5791bc4e5844c3bf03b92
Cosmits <salge.cos@gmail.com>
2023-04-13 11:49:19 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,41 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const divBoxesEl = document.querySelector('#boxes');
+const inputEl = document.querySelector('input');
+
+let globalCounter = 0;
+
+
+createBtn.addEventListener('click', () => createBoxes(Number(inputEl.value)))
+
+
+function createBoxes(amount) {
+
+  let max__i = 0;
+
+  for (let i = globalCounter; i < amount + globalCounter; i += 1) {
+
+    let num = i * 10 + 30;
+    max__i = i;
+
+    const divNewEl = document.createElement('div');
+    divNewEl.style.width = `${num}px`;
+    divNewEl.style.height = `${num}px`;
+
+    divNewEl.style.backgroundColor = getRandomHexColor();
+    divBoxesEl.append(divNewEl);
+  }
+  globalCounter = globalCounter + (max__i - globalCounter + 1);
+  
+}
+
+function destroyBoxes() {
+  document.getElementById('boxes').innerHTML = '';
+  globalCounter = 0;
+}
+
+destroyBtn.addEventListener('click', destroyBoxes)
